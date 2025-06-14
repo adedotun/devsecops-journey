ebadf5c70393c27b4ea051f26c2d5189186844d2
adedotun <dotun.ogunwuyi71@gmail.com>
2025-06-14 15:31:16 -0400

    updated dockerfile

--- day2-docker-python/app.py
+++ day2-docker-python/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello, DevSecOps from Python!"
+def home():
+    return "Hello, Secure Flask!"
 
-if __name__ == '__main__':
+@app.route('/health')
+def health():
+    return "OK", 200
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
-
